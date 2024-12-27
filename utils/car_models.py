67d263d4ec46bc6617aa37adc000e743abe7e226
Szymon Kowalik <szymon.kowalik44@gmail.com>
2024-12-27 23:49:05 +0100

    Add GT2 cars and new tracks

--- utils/car_models.py
+++ utils/car_models.py
@@ -9,6 +9,7 @@ class Car:
 
 
 cars = [
+    # GT3 Cars
     [12, 'Aston Martin', 'V12 Vantage GT3', '2013', 'GT3'],
     [20, 'Aston Martin', 'V8 Vantage GT3', '2019', 'GT3'],
     [3, 'Audi', 'R8 LMS', '2015', 'GT3'],
@@ -36,9 +37,11 @@ cars = [
     [10, 'Nissan', 'GT-R Nismo GT3', '2015', 'GT3'],
     [6, 'Nissan', 'GT-R Nismo GT3', '2018', 'GT3'],
     [0, 'Porsche', '991 GT3 R', '2018', 'GT3'],
-    [23, 'Porsche', '991 II GT3 R', '2019', 'GT3'],
+    [23, 'Porsche', '911 II GT3 R', '2019', 'GT3'],
     [34, 'Porsche', '992 GT3 R', '2023', 'GT3'],
     [13, 'Reiter Engineering', 'R-EX GT3', '2017', 'GT3'],
+
+    # GT4 Cars
     [50, 'Alpine', 'A110 GT4', '2018', 'GT4'],
     [51, 'Aston Martin', 'Vantage GT4', '2018', 'GT4'],
     [52, 'Audi', 'R8 LMS GT4', '2018', 'GT4'],
@@ -50,13 +53,24 @@ cars = [
     [59, 'McLaren', '570S GT4', '2016', 'GT4'],
     [60, 'Mercedes-AMG', 'GT4', '2016', 'GT4'],
     [61, 'Porsche', '718 Cayman GT4 Clubsport', '2019', 'GT4'],
+
+    # GT2 Cars
+    [80, 'Audi', 'R8 LMS GT2', '2023', 'GT2'],
+    [82, 'KTM', 'X-Bow GT2', '2023', 'GT2'],
+    [83, 'Maserati', 'MC20 GT2', '2023', 'GT2'],
+    [84, 'Mercedes-AMG', 'GT2', '2023', 'GT2'],
+    [85, 'Porsche', '911 GT2 RS Clubsport Evo', '2023', 'GT2'],
+    [86, 'Porsche', '935', '2019', 'GT2'],
+
+    # GTC Cars
     [26, 'Ferrari', '488 Challenge Evo', '2020', 'GTC'],
     [18, 'Lamborghini', 'Huracan Super Trofeo', '2015', 'GTC'],
     [29, 'Lamborghini', 'Huracan Super Trofeo EVO2', '2021', 'GTC'],
     [9, 'Porsche', '991 II GT3 Cup', '2017', 'GTC'],
     [28, 'Porsche', '992 GT3 Cup', '2021', 'GTC'],
+
+    # TCX Cars
     [27, 'BMW', 'M2 CS Racing', '2020', 'TCX']
 ]
-
 CAR_MODELS = {car_id: Car(car_id, manufacturer, model, year, group)
               for car_id, manufacturer, model, year, group in cars}
